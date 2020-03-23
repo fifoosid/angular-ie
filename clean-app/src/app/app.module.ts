f305f61c6016053831a41eda87e23628cfb7b077
Vladislav Tasev <vladislav.tasev@sap.com>
2020-03-23 09:40:46 +0200

    new changes

--- clean-app/src/app/app.module.ts
+++ clean-app/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import "@ui5/webcomponents-base/dist/features/browsersupport/IE11WithWebComponentsPolyfill.js";
+
+import "@ui5/webcomponents/dist/DatePicker.js";
+import "@ui5/webcomponents/dist/Switch.js";
+
 @NgModule({
   declarations: [
     AppComponent
